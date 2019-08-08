9770b52c90bcabe0e218ad2e035e32cf93a4f59d
Matt Farrow <matt.farrow@siriusopensource.com>
2019-08-08 15:12:21 +0100

    Initial commit
    Version 10

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php define('VERSION', 10); ?>
+<!doctype html>
+<html>
+  <head>
+    <title>Test version <?php print VERSION; ?></title>
+    <style>
+      dl {
+        border: 3px double #ccc;
+        padding: 0.5em;
+      }
+      dt {
+        float: left;
+        clear: left;
+        width: 100px;
+        text-align: right;
+        font-weight: bold;
+        color: green;
+      }
+      dt::after {
+        content: ":";
+      }
+      dd {
+        margin: 0 0 0 110px;
+        padding: 0 0 0.5em 0;
+      }
+    </style>
+  </head>
+  <body>
+    <h1>Test version <?php print VERSION; ?></h1>
+    <dl>
+      <dt>Version</dt>
+      <dd><?php print VERSION; ?></dd>
+    </dl>
+    <p>This is a test.</p>
+    <pre>
+      <?php var_dump($_SERVER); ?>
+    </pre>
+  </body>
+</html>
