d7ad36dc7425185184233ea28f02ae630f757803
Matt Farrow <matt.farrow@siriusopensource.com>
2019-08-09 09:00:22 +0100

    Version 26

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php define('VERSION', 26); ?>
+<?php define('VERSION', 27); ?>
 <?php $site = file_get_contents('/home/ubuntu/site.txt'); ?>
 <?php $commit = file_get_contents('/home/ubuntu/commit.txt'); ?>
 <?php $asg_ami = file_get_contents('/home/ubuntu/asg_ami.txt'); ?>
@@ -7,6 +7,10 @@
   <head>
     <title>Test version <?php print VERSION; ?></title>
     <style>
+      body {
+        margin-top: 0;
+        padding-top: 0;
+      }
       dl {
         border: 3px double #ccc;
         padding: 0.5em;
@@ -26,9 +30,13 @@
         margin: 0 0 0 110px;
         padding: 0 0 0.5em 0;
       }
+      #header {
+        display: block;
+      }
     </style>
   </head>
   <body>
+    <img src="/shared-files/le_header_1x_0.jpg" id="header">
     <h1>Test version <?php print VERSION; ?></h1>
     <dl>
       <dt>Version</dt>
@@ -41,7 +49,6 @@
       <dd><?php print $asg_ami; ?></dd>
     </dl>
     <p>This is a test.</p>
-    <img src="/shared-files/logo.png">
     <pre>
       <?php var_dump($_SERVER); ?>
     </pre>
